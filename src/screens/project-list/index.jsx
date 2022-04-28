cc60682113a2e52ac2bd54f6f1c3bd796396864a
Casual player <emailofgmz@gmail.com>
2022-04-28 18:20:01 +0800

    qs,utils

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,6 +1,8 @@
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useState, useEffect } from "react";
+import * as qs from "qs";
+import { cleanObject } from "utils";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -29,19 +31,21 @@ export const ProjectListScreen = () => {
   ]);
   const [list, setList] = useState([]);
   useEffect(() => {
-    fetch(`${apiUrl}/projects`).then(async (res) => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (res) => {
+        if (res.ok) {
+          setList(await res.json());
+        }
+      }
+    );
+  }, [param]);
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (res) => {
       if (res.ok) {
-        setList(await res.json());
+        setUsers(await res.json());
       }
     });
-  }, [param]);
-  //   useEffect(() => {
-  //     fetch(`${apiUrl}/users`).then(async (res) => {
-  //       if (res.ok) {
-  //         setUsers(await res.json());
-  //       }
-  //     });
-  //   }, []);
+  }, []);
   return (
     <div>
       <SearchPanel param={param} setParam={setParam} users={users} />
