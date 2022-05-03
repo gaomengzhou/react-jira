ec0b49ad5f26eb381d66f4ee441aafc9f0002730
Casual player <emailofgmz@gmail.com>
2022-05-03 09:17:41 +0800

    登录注册

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { AuthenticatedApp } from "authenticated";
+import { useAuth } from "context/auth-context";
 import React from "react";
+import { UnathenticatedApp } from "unauthenticated-app";
 import "./App.css";
-import { ProjectListScreen } from "screens/project-list";
-import { LoginScreen } from "screens/login";
 
 function App() {
+  const { user } = useAuth();
   return (
     <div className="App">
-      <LoginScreen />
-      {/*<ProjectListScreen />*/}
+      {user ? <AuthenticatedApp /> : <UnathenticatedApp />}
     </div>
   );
 }
--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ReactNode, useContext, useState } from "react";
 import * as auth from "auth-provider";
 import { User } from "screens/project-list/search-panel";
 
@@ -6,14 +6,36 @@ interface AuthForm {
   username: string;
   password: string;
 }
-const AuthContext = React.createContext(undefined);
+interface AuthContextProps {
+  user: User | null;
+  login: (form: AuthForm) => Promise<void>;
+  register: (form: AuthForm) => Promise<void>;
+  logout: () => Promise<void>;
+}
+const AuthContext = React.createContext<AuthContextProps | undefined>(
+  undefined
+);
 AuthContext.displayName = "AuthContext";
 
-export const AuthProvider = () => {
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
   const login = (form: AuthForm) =>
     auth.login(form).then((res) => setUser(res));
   const register = (form: AuthForm) =>
     auth.register(form).then((res) => setUser(res));
   const logout = () => auth.logout().then(() => setUser(null));
+  return (
+    <AuthContext.Provider
+      children={children}
+      value={{ user, login, register, logout }}
+    />
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth必须在AuthProvider中使用");
+  }
+  return context;
 };
