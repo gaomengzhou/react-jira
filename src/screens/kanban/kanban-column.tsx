d7863365b27ba44479094a0c397c514278c87964
Casual player <emailofgmz@gmail.com>
2022-05-12 14:48:47 +0800

    拖拽

--- src/screens/kanban/kanban-column.tsx
+++ src/screens/kanban/kanban-column.tsx
@@ -43,14 +43,17 @@ const TaskCard = ({ task }: { task: Task }) => {
   );
 };
 
-export const KanbanColumn = ({ kanban }: { kanban: Kanban }) => {
+export const KanbanColumn = React.forwardRef<
+  HTMLDivElement,
+  { kanban: Kanban }
+>(({ kanban, ...props }, ref) => {
   const { data: allTasks } = useTasks(useTasksSearchParams());
   const tasks = allTasks?.filter((task) => task.kanbanId === kanban.id);
   return (
-    <Container>
+    <Container {...props} ref={ref}>
       <Row between={true}>
         <h3>{kanban.name}</h3>
-        <More kanban={kanban} />
+        <More kanban={kanban} key={kanban.id} />
       </Row>
       <TasksContainer>
         {tasks?.map((task, i) => (
@@ -60,7 +63,7 @@ export const KanbanColumn = ({ kanban }: { kanban: Kanban }) => {
       </TasksContainer>
     </Container>
   );
-};
+});
 
 const More = ({ kanban }: { kanban: Kanban }) => {
   const { mutateAsync } = useDeleteKanban(useKanbansQueryKey());
